e85a1f93586dffcddadec0d022ddbfac633eff82
ShubhamThakur025 <thakur0805@gmail.com>
2023-10-20 19:18:24 +0530

    Cric Pro Assignment

---
+++ script.js
@@ -0,0 +1,15 @@
+//FUnction defined for the
+function popup(index){
+    let elem = document.getElementsByClassName('VS')
+    elem[index].style.display= 'flex';
+}
+
+//Event-Listener defined for the pop-up to disappear when clicked on the screen
+window.addEventListener('click', function(event){
+    let elem = document.getElementsByClassName('VS');
+    for (let i = 0; i < elem.length; i++) {
+        if (elem[i].contains(event.target)) {
+            elem[i].style.display = 'none';
+        }
+    }
+})
